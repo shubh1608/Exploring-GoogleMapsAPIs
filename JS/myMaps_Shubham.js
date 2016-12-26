d3a42123781b61b5a667bb36897bb787ccb29ea4
Shubham <shubhampatel1608@gmail.com>
2016-12-26 23:13:57 +0530

    Added Directions Panel

--- JS/myMaps_Shubham.js
+++ JS/myMaps_Shubham.js
@@ -1,8 +1,9 @@
 ﻿var myMaps = {
+    gblMapRef:null,
     Init: function () {
         var me = myMaps;
-        me.BindEvents();
         me.InitializeMaps();
+        me.BindEvents();
     },
 
     BindEvents: function () {
@@ -12,11 +13,43 @@
             me.FindRoute($("#txtSource").val(), $("#txtDestination").val());
             //me.GetGeoCodeForAddress($("#txtSource").val(), $("#txtDestination").val());
         });
+
+        me.gblMapRef.addListener('click', function (event) {
+            me.PlaceMarker(event.latLng);
+        });
+
+        
     },
 
-    FindRoute: function (origin,destination) {
+    
+
+    PlaceMarker: function (location) {
         var me = myMaps;
-        
+        var marker = new google.maps.Marker({
+            position: location,
+            map: me.gblMapRef
+        });
+
+
+        marker.addListener("dblclick", function () {
+            marker.setMap(null);
+        });
+
+        marker.addListener("click", function () {
+            //marker.setMap(null);
+            var infowindow = new google.maps.InfoWindow({
+                content: $("#divInfoWindowContent").html(),
+                maxWidth:300
+            });
+            infowindow.open(me.gblMapRef, marker);
+        });
+
+    },
+
+
+    FindRoute: function (origin, destination) {
+        var me = myMaps;
+
         var directionsService = new google.maps.DirectionsService();
         var directionsDisplay = new google.maps.DirectionsRenderer();
         directionsDisplay.setMap(null);
@@ -29,8 +62,10 @@
         directionsService.route(request, function (response, status) {
             if (status == 'OK') {
                 directionsDisplay.setDirections(response);
+                directionsDisplay.setPanel(document.getElementById('divDirectionPanel'));
             }
         });
+        me.BindEvents();
     },
 
     GetGeoCodeForAddress: function (source, destination) {
@@ -40,15 +75,15 @@
         var addressArray = [];
         geocoder.geocode({ 'address': source }, function (results, status) {
             if (status == 'OK') {
-                var addObjSrc={};
-                addObjSrc.Latitude=results[0].geometry.location.lat();
-                addObjSrc.Longitude=results[0].geometry.location.lng();
+                var addObjSrc = {};
+                addObjSrc.Latitude = results[0].geometry.location.lat();
+                addObjSrc.Longitude = results[0].geometry.location.lng();
                 addressArray.push(addObjSrc);
                 geocoder.geocode({ 'address': destination }, function (results, status) {
                     if (status == 'OK') {
-                        var addObjDest={};
-                        addObjDest.Latitude=results[0].geometry.location.lat();
-                        addObjDest.Longitude=results[0].geometry.location.lng();
+                        var addObjDest = {};
+                        addObjDest.Latitude = results[0].geometry.location.lat();
+                        addObjDest.Longitude = results[0].geometry.location.lng();
                         addressArray.push(addObjDest);
                         //alert("Source: " + addressArray[0].Latitude + ", " + addressArray[0].Longitude + "\n" + "Destination:" + addressArray[1].Latitude + ", " + addressArray[1].Longitude);
                         me.FindRoute(addressArray);
@@ -62,20 +97,21 @@
         });
     },
 
-    CreateMap:function(latitude,longitude){
+    CreateMap: function (latitude, longitude) {
         var me = myMaps;
         var map = new google.maps.Map($("#divMap")[0], {
             center: { lat: latitude, lng: longitude },
             scrollwheel: false,
             zoom: 15
         });
+        me.gblMapRef = map;
         return map;
     },
 
     InitializeMaps: function () {
         var me = myMaps;
         var map = me.CreateMap(21.1458, 79.0882);
-        
+
         var marker = new google.maps.Marker({ position: { lat: 21.1458, lng: 79.0882 } });
         marker.setMap(map);
 
@@ -88,3 +124,11 @@
 $(function () {
     myMaps.Init();
 });
+
+
+function SaveNote(event) {
+    event.preventDefault();
+    var jsonObj = {
+        
+    };
+}
